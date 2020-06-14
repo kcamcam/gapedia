6523f5a18d0bdf47ec87d71865e8e81dca34fef5
Kevin Camellini <kevincamellini@gmail.com>
2020-06-14 16:44:45 -0400

    adds 3:1 up/downvote ratio

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,19 +1,21 @@
 class VoteController < ApplicationController
-	def upvote
-		@vote = Vote.find(1)
-		@vote.upvote = @vote.upvote+1
-		@vote.update(vote_params)
-		redirect_to root_path
-	end
-	def downvote
-		@vote = Vote.find(1)
-		@vote.downvote = @vote.downvote+1
-		@vote.update(vote_params)
-		redirect_to root_path
-	end	
-	def vote_params
-		params.permit(:upvote, :downvote)
-	end  	
+  def upvote
+    @vote = Vote.first
+    @vote.upvote = @vote.upvote + 1
+    @vote.update(vote_params)
+    redirect_to root_path
+  end
+
+  def downvote
+    @vote = Vote.find(1)
+    @vote.downvote = @vote.downvote + 1
+    @vote.update(vote_params)
+    redirect_to root_path
+  end
+
+  def vote_params
+    params.permit(:upvote, :downvote)
+  end
 end
-# reset with > 
+# reset with >
 # @vote = Vote.find(1);@vote.downvote = 0;@vote.upvote = 1;@vote.save;
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
-	def index
-		@vote = Vote.find(1)
-		@total = @vote.upvote+@vote.downvote
-		@accuracy = ((@vote.upvote.to_f/(@total))*100).round(0)
-	end
+  def index
+    @vote = Vote.first
+    @total = @vote.upvote + @vote.downvote
+    @upvote = (@vote.upvote * 3).to_f
+    @accuracy = ((@upvote / (@upvote + @vote.downvote)) * 100).round(0)
+  end
 end
